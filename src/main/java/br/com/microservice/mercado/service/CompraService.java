f15fda79a29c579e582e975488ae291f2ede9bb3
AlanSSoares92 <alan_s_soares@outlook.com>
2022-10-22 00:08:44 -0300

    create: criação de dtos do lado do cliente

--- src/main/java/br/com/microservice/mercado/service/CompraService.java
+++ src/main/java/br/com/microservice/mercado/service/CompraService.java
@@ -1,10 +1,22 @@
 package br.com.microservice.mercado.service;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
 import br.com.microservice.mercado.controller.dto.CompraDTO;
+import br.com.microservice.mercado.controller.dto.InfoFornecedorDTO;
 
 public class CompraService {
 	
 	public void compra(CompraDTO compra) {
+		
+		RestTemplate client = new RestTemplate();
+		ResponseEntity<InfoFornecedorDTO> exchange =
+		client.exchange("http://localhost:8081/" + compra.getEndereco().getEstado(), HttpMethod.GET, null, InfoFornecedorDTO.class);
+		
+		
+		System.out.println(exchange.getBody().getEndereco());
 		
 	}
 
