5e9c0b857173b0b5804bd11beeaa9645386ff786
AlanSSoares92 <alan_s_soares@outlook.com>
2022-10-22 22:56:38 -0300

    ajustes

--- src/main/java/br/com/microservice/mercado/service/CompraService.java
+++ src/main/java/br/com/microservice/mercado/service/CompraService.java
@@ -10,11 +10,17 @@ import br.com.microservice.mercado.controller.dto.InfoFornecedorDTO;
 public class CompraService {
 	
 	public void compra(CompraDTO compra) {
-		
+		//RestTemplate é uma ferramenta do Spring para requisições HTTP que são síncronas, ou seja, a cada requisição, 
+		//a execução da aplicação pára, esperando uma resposta
 		RestTemplate client = new RestTemplate();
+		//ResponseEntity: significa representar toda a resposta HTTP. Você pode controlar qualquer coisa que aconteça: código de status, cabeçalhos e corpo. 
+		//Trabalhando com microservice, ResponseEntity para enviar resposta completa, com status, com cabeçalho e corpo.
 		ResponseEntity<InfoFornecedorDTO> exchange =
-		client.exchange("http://localhost:8081/" + compra.getEndereco().getEstado(), HttpMethod.GET, null, InfoFornecedorDTO.class);
-		
+		client.exchange("http://localhost:8081/" + compra.getEndereco().getEstado(), 
+				HttpMethod.GET, null, InfoFornecedorDTO.class);
+//		O interceptor HTTP Method "troca" o método HTTP da chamada corrente. Por exemplo, suponhamos que a chamada a um recurso de uma API cadastrada ( /stores/{id} ) seja do tipo GET; porém,
+//		o seu servidor suporta apenas chamadas do tipo POST, 
+//		por isso este é o método cadastrado no Manager.
 		
 		System.out.println(exchange.getBody().getEndereco());
 		
